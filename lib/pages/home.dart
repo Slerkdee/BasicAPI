9f136dc628b8c6e2d8269ed2f3715c626c1198c4
Slerkdee <124506782+Slerkdee@users.noreply.github.com>
2023-07-15 14:47:26 +0700

    Add files via upload

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,12 @@
 import 'dart:convert';
-
 import 'package:cardview/pages/detail.dart';
 import 'package:flutter/material.dart';
+
+/*------- package สำหรับ request จาก Web ใช้  3 package -----*/
+import 'package:http/http.dart' as http;
+import 'dart:async';
+//import 'dart:convert';  ปล.เรียกใช้ก่อนหน้านี้แล้ว
+/*------- End package สำหรับ request จาก Web ใช้  3 package -----*/
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,30 +25,46 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
         ),
       ),
+      // body: Padding(
+      //     padding: EdgeInsets.all(10),
+      //     /*------- ชุดคำสั่งสำหรับอ่านไฟล์ Json -----*/
+      //     child: FutureBuilder(
+      //       builder: (context, snapshot) {
+      //         var data = json.decode(snapshot.data
+      //             .toString()); //[{ลิง......},{สุนัขไทยหลังอาน...},{}]
+      //         return ListView.builder(
+      //           itemBuilder: (BuildContext context, int index) {
+      //             return MyBox(data[index]['title'], data[index]['subtitle'],
+      //                 data[index]['image_url'], data[index]['detail']);
+      //           },
+      //           itemCount: data.length,
+      //         );
+      //       },
+      //       future:DefaultAssetBundle.of(context).loadString('assets/data.json'),
+      //     )),
+      // /*------- End อ่านไฟล์ Json -----*/
+
       body: Padding(
           padding: EdgeInsets.all(10),
-          /*------- ชุดคำสั่งสำหรับอ่านไฟล์ Json -----*/
+          /*------- ชุดคำสั่งสำหรับอ่านไฟล์ Json from Web http request-----*/
           child: FutureBuilder(
-            builder: (context, snapshot) {
-              var data = json.decode(snapshot.data
-                  .toString()); //[{ลิง......},{สุนัขไทยหลังอาน...},{}]
+            builder: (context,AsyncSnapshot snapshot) {
               return ListView.builder(
                 itemBuilder: (BuildContext context, int index) {
-                  return MyBox(data[index]['title'], data[index]['subtitle'],
-                      data[index]['image_url'],data[index]['detail']);
+                  return MyBox(snapshot.data[index]['title'], snapshot.data[index]['subtitle'],
+                      snapshot.data[index]['image_url'], snapshot.data[index]['detail']);
                 },
-                itemCount: data.length,
+                itemCount:snapshot.data.length,
               );
             },
-            future:
-                DefaultAssetBundle.of(context).loadString('assets/data.json'),
+            future: getData(),
           )),
-          /*------- End อ่านไฟล์ Json -----*/
+      /*------- End ชุดคำสั่งสำหรับอ่านไฟล์ Json from Web http request-----*/
     );
   }
 
-  Widget MyBox(String title, String subtitle, String image_url,String detail) {
-     /*------- สร้าง ตัวแปร รับค่าจาก Json เพื่อส่งไปอีกไฟล์-----*/
+  Widget MyBox(String title, String subtitle, String image_url, String detail) {
+    /*------- สร้าง ตัวแปร รับค่าจาก Json เพื่อส่งไปอีกไฟล์-----*/
     var v1, v2, v3, v4;
     v1 = title;
     v2 = subtitle;
@@ -81,7 +102,8 @@ class _HomePageState extends State<HomePage> {
               Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => DetailPage(v1,v2,v3,v4),//ส่งค่าที่อ่านได้ไปที่ไฟล์ Detail
+                    builder: (context) => DetailPage(
+                        v1, v2, v3, v4), //ส่งค่าที่อ่านได้ไปที่ไฟล์ Detail
                   ));
             },
             child: Text('อ่านต่อ'),
@@ -90,4 +112,13 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+ /*------ function สำหรับ get ไฟล์จาก github ------*/
+  Future getData() async {
+    //https://raw.githubusercontent.com/Slerkdee/BasicAPI/main/data.json
+    var url = Uri.http('raw.githubusercontent.com','/Slerkdee/BasicAPI/main/data.json');
+    var response = await http.get(url);
+    var result = json.decode(response.body);
+    return result;
+  }
+  /*------ End function สำหรับ get ไฟล์จาก github ------*/
 }
